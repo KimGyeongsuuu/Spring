0e5b5bd04055a2ff919a0aede6f1dea8bc08ebd3
Kim gyeong su <sk62628504@gmail.com>
2022-10-08 20:55:54 +0900

    spring

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -22,9 +22,16 @@ public class MemberService {
     }
 
     public long join(Member member){
-        validateDuplicateMember(member);
-        memberRepository.save(member);
-        return member.getId();
+        long start = System.currentTimeMillis();
+        try{
+            validateDuplicateMember(member);
+            memberRepository.save(member);
+            return member.getId();
+        }finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join = "+timeMs+"ms");
+        }
     }
 
     private void validateDuplicateMember(Member member) {
@@ -34,7 +41,14 @@ public class MemberService {
                 });
     }
     public List<Member> findMembers(){
-        return memberRepository.findAll();
+        long start = System.currentTimeMillis();
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers " + timeMs + "ms");
+        }
     }
     public Optional<Member> findOne(Long memberId){
         return memberRepository.findById(memberId);
