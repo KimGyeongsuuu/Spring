bc3dfa066eff60877d86fcfa30676ef9e5cadfeb
Kim gyeong su <sk62628504@gmail.com>
2022-11-07 16:06:42 +0900

    configuration

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -21,4 +21,8 @@ public class OrderServiceImpl implements OrderService{
 
         return new Order(memberId, itemName, itemPrice, discountprice);
     }
+
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
 }
