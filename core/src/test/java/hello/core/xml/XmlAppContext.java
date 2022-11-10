2dbb9e695b2d23635b425bb3456f575398905144
Kim gyeong su <sk62628504@gmail.com>
2022-11-10 20:52:16 +0900

    bean scope

--- core/src/test/java/hello/core/xml/XmlAppContext.java
+++ core/src/test/java/hello/core/xml/XmlAppContext.java
@@ -1,19 +1,13 @@
 package hello.core.xml;
-
 import hello.core.member.MemberService;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
-import org.springframework.test.context.support.GenericXmlContextLoader;
-
-import java.lang.reflect.GenericArrayType;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
+import static org.assertj.core.api.Assertions.*;
 public class XmlAppContext {
-
     @Test
-    void xmlAppContext(){
+    void xmlAppContext() {
+
         ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
 
         MemberService memberService = ac.getBean("memberService",
