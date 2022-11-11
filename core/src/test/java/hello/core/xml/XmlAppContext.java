24d4b12ef460c8740e7692667e042901415a0efe
Kim gyeong su <sk62628504@gmail.com>
2022-11-11 21:49:47 +0900

    test

--- core/src/test/java/hello/core/xml/XmlAppContext.java
+++ core/src/test/java/hello/core/xml/XmlAppContext.java
@@ -3,15 +3,17 @@ import hello.core.member.MemberService;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
-import static org.assertj.core.api.Assertions.*;
-public class XmlAppContext {
+import org.assertj.core.api.Assertions.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class XmlAppContext{
+
     @Test
     void xmlAppContext() {
 
         ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
-
-        MemberService memberService = ac.getBean("memberService",
-                MemberService.class);
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
         assertThat(memberService).isInstanceOf(MemberService.class);
     }
 }
