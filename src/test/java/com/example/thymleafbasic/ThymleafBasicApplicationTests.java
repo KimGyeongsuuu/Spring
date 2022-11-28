9df2b58ae73b3e1fe8ff709b0301daa02eab52ec
Kim gyeong su <sk62628504@gmail.com>
2022-11-28 16:01:02 +0900

    first commit

---
+++ src/test/java/com/example/thymleafbasic/ThymleafBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thymleafbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymleafBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
