dc67283a8c6c4adce72eb2ea73a9387723c222a4
Kim gyeong su <sk62628504@gmail.com>
2022-11-29 20:59:50 +0900

    basic

--- thymeleaf-basic/src/main/java/com/example/thymeleafbasic/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/com/example/thymeleafbasic/basic/BasicController.java
@@ -1,10 +1,13 @@
 package com.example.thymeleafbasic.basic;
 import lombok.Data;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -37,6 +40,68 @@ public class BasicController {
         model.addAttribute("users", list);
         model.addAttribute("userMap", map);
         return "basic/variable";
+    }
+
+    @GetMapping("/basic-objects")
+    public String basicObjects(HttpSession session){
+        session.setAttribute("sessionData","Hello Session");
+        return "basic/basic-objects";
+    }
+
+    @GetMapping("/date")
+    public String date (Model model){
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        return "basic/date";
+    }
+    @GetMapping("/link")
+    public String link(Model model){
+        model.addAttribute("param1","data1");
+        model.addAttribute("param2","data2");
+        return "basic/link";
+    }
+    @GetMapping("/literal")
+    public String literal(Model model){
+        model.addAttribute("data","Spring!!");
+        return "basic/literal";
+    }
+    @GetMapping("/operation")
+    public String operation(Model model){
+        model.addAttribute("nullData",null);
+        model.addAttribute("data","spring!");
+        return "basic/operation";
+    }
+    @GetMapping("/attribute")
+    public String attribute(){
+        return "basic/attribute";
+    }
+
+    @Component("helloBean")
+    static class HelloBean{
+        public String hello(String data){
+            return "hello "+data;
+        }
+    }
+
+    @GetMapping("/each")
+    public String each(Model model){
+        addUsers(model);
+        return "basic/each";
+    }
+
+    @GetMapping("/condition")
+    public String condition(Model model){
+        addUsers(model);
+        return "basic/condition";
+    }
+
+
+    private void addUsers(Model model){
+        List<User> list = new ArrayList<>();
+        list.add(new User("UserA",10));
+        list.add(new User("UserB",20));
+        list.add(new User("UserC",30));
+
+        model.addAttribute("users",list);
     }
     @Data
     static class User {
