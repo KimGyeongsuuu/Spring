752a98186359b6c505531cefbaccbf486f3aafd7
Kim gyeong su <sk62628504@gmail.com>
2022-11-30 09:23:09 +0900

    mvc2_basic

--- thymeleaf-basic/src/main/java/com/example/thymeleafbasic/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/com/example/thymeleafbasic/basic/BasicController.java
@@ -94,6 +94,24 @@ public class BasicController {
         return "basic/condition";
     }
 
+    @GetMapping("/comments")
+    public String comments(Model model){
+        model.addAttribute("data","spring!");
+        return "basic/comments";
+    }
+
+    @GetMapping("/block")
+    public String block(Model model){
+        addUsers(model);
+        return "basic/block";
+    }
+
+    @GetMapping("/javascript")
+    public String javasript(Model model){
+        model.addAttribute("user",new User("userA",10));
+        return "basic/javascript";
+    }
+
 
     private void addUsers(Model model){
         List<User> list = new ArrayList<>();
