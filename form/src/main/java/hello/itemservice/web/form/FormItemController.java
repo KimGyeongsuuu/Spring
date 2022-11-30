aa91f16d6317c0504f744b51cd3c8d5994f7c328
Kim gyeong su <sk62628504@gmail.com>
2022-11-30 14:32:18 +0900

    mvc2_button

--- form/src/main/java/hello/itemservice/web/form/FormItemController.java
+++ form/src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -3,6 +3,7 @@ package hello.itemservice.web.form;
 
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
+import hello.itemservice.domain.item.ItemType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -11,7 +12,9 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -19,6 +22,20 @@ import java.util.List;
 @RequiredArgsConstructor
 public class FormItemController {
     private final ItemRepository itemRepository;
+
+    @ModelAttribute("regions")
+    public Map<String, String> regions(){
+        Map<String, String> regions = new LinkedHashMap<>();
+        regions.put("SEOUL","서울");
+        regions.put("BUSAN","부산");
+        regions.put("JEJU","제주");
+        return regions;
+    }
+
+    @ModelAttribute("itemTypes")
+    public ItemType[] itemTypes(){
+        return ItemType.values();
+    }
 
     @GetMapping
     public String items(Model model){
@@ -38,6 +55,7 @@ public class FormItemController {
     @GetMapping("/add")
     public String addForm(Model model){
         model.addAttribute("item",new Item());
+
         return "basic/addForm";
     }
 
@@ -46,6 +64,8 @@ public class FormItemController {
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
 
         log.info("item.open = {}",item.getOpen());
+        log.info("regions.open = {}",item.getRegions());
+        log.info("item.itemType = {}",item.getItemType());
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId",savedItem.getId());
         redirectAttributes.addAttribute("status",true);
@@ -56,6 +76,7 @@ public class FormItemController {
     public String editForm(@PathVariable Long itemId, Model model){
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item",item);
+
         return "basic/editForm";
     }
 
@@ -64,6 +85,7 @@ public class FormItemController {
         itemRepository.update(itemId, item);
         return "redirect:/basic/items/{itemId}";
     }
+
 
     @PostConstruct
     public void init() {
