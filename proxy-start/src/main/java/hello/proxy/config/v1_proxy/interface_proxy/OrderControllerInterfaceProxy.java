dfa112976dd06a79b1a22c523ba04a591f20fc40
Kim gyeong su <sk62628504@gmail.com>
2023-01-31 22:44:43 +0900

    InterfaceProxy

--- proxy-start/src/main/java/hello/proxy/config/v1_proxy/interface_proxy/OrderControllerInterfaceProxy.java
+++ proxy-start/src/main/java/hello/proxy/config/v1_proxy/interface_proxy/OrderControllerInterfaceProxy.java
@@ -1,32 +1,34 @@
 package hello.proxy.config.v1_proxy.interface_proxy;
 
-import hello.proxy.app.v1.OrderRepositoryV1;
+import hello.proxy.app.v1.OrderControllerV1;
 import hello.proxy.trace.TraceStatus;
 import hello.proxy.trace.logtrace.LogTrace;
 import lombok.RequiredArgsConstructor;
 
-
 @RequiredArgsConstructor
-public class OrderRepositoryInterfaceProxy implements OrderRepositoryV1 {
+public class OrderControllerInterfaceProxy implements OrderControllerV1 {
 
-    private final OrderRepositoryV1 target;
+    private final OrderControllerV1 target;
     private final LogTrace logTrace;
 
     @Override
-    public void save(String itemId) {
+    public String request(String itemId) {
 
         TraceStatus status = null;
-
         try{
-            status = logTrace.begin("OrderRepository.save()");
-            target.save(itemId);
+            status = logTrace.begin("OrderController.request() 실행");
+            String result = target.request(itemId);
             logTrace.end(status);
+            return result;
+
         }catch (Exception e){
             logTrace.exception(status, e);
             throw e;
         }
+    }
 
-
-
+    @Override
+    public String noLog() {
+        return target.noLog();
     }
 }
