62bd5a2b85d8779851e2923b44c296eefe15b0b4
Kim gyeong su <sk62628504@gmail.com>
2023-01-31 15:39:40 +0900

    DecoratorPattern

---
+++ proxy-start/src/test/java/hello/proxy/pureproxy/decorator/DecoratePatternTest.java
@@ -0,0 +1,29 @@
+package hello.proxy.pureproxy.decorator;
+
+import hello.proxy.pureproxy.decorator.code.Component;
+import hello.proxy.pureproxy.decorator.code.DecoratorPatternClient;
+import hello.proxy.pureproxy.decorator.code.MessageDecorator;
+import hello.proxy.pureproxy.decorator.code.RealComponent;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+
+@Slf4j
+public class DecoratePatternTest {
+
+    @Test
+    void noDecorator(){
+        Component realComponent = new RealComponent();
+        DecoratorPatternClient decoratorPatternClient = new DecoratorPatternClient(realComponent);
+        decoratorPatternClient.execute();
+    }
+
+    @Test
+    void Decorator1(){
+        RealComponent realComponent = new RealComponent();
+        MessageDecorator messageDecorator = new MessageDecorator(realComponent);
+        DecoratorPatternClient decoratorPatternClient = new DecoratorPatternClient(messageDecorator);
+
+        decoratorPatternClient.execute();
+    }
+}
